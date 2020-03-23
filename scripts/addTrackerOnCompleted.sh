a27d11aa3406cc07bbbc38f51027b80030b8eca0
Guillaume <guillaume.savignan@viavoo.com>
2020-03-23 20:34:23 +0100

    add script

---
+++ scripts/addTrackerOnCompleted.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+transmission-remote -n "usr:psswd" -t "${TR_TORRENT_ID}" --tracker-add "http://ygg.peer2peer.cc:8080/DnnISwk4JxKYnBpvx3shJrBIonm67DEO/announce"
+# to add script
+# transmission-remote -n usr:psswrd --torrent-done-script /path/to/addTrackerOnCompleted.sh
+
+
+
+
+
